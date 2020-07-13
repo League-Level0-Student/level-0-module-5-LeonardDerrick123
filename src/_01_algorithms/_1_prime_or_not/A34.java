71b79801811a2023b8d9226c483532017dc62949
root <root@9c6921ac1f28>
2020-07-13 18:28:08 +0000

    prime or not

--- src/_01_algorithms/_1_prime_or_not/A34.java
+++ src/_01_algorithms/_1_prime_or_not/A34.java
@@ -1,8 +1,18 @@
 package _01_algorithms._1_prime_or_not;
 
+import javax.swing.JOptionPane;
+
 public class A34 {
 	public static void main(String args[]){
-	JOPtionPane	
+	String numbers=JOptionPane.showInputDialog("tellish me thy number to seeith if prime or noitith if notith off with ye head")	;
+	int prime=Integer.parseInt(numbers);
+	for (int i = 2; i < prime/2; i++) {
+	if (prime%i == 0) {
+		JOptionPane.showMessageDialog(null, "off with ye head");
+	System.exit(0);
+	}
 		
+	}
+JOptionPane.showMessageDialog(null, "prime");	
 	}
 }
