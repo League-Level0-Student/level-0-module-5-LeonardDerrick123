097aabd285495440612a8a7a3f13cb328fdd8545
LeonardDerrick123 <50834186+LeonardDerrick123@users.noreply.github.com>
2020-07-17 18:35:10 +0000

    Add files via upload

--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -2,54 +2,62 @@ package _02_nested_loops._1_snowflake;
 
 import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class Snowflake {
-	
+
 	public static void main(String[] args) {
 
 		// #1. Make a new Robot
-
+		Robot N7 = new Robot();
 
 		// #2. Set your robot’s position to x=300 and y=300
-
+		N7.setX(300);
+		N7.setY(300);
 
 		// #3. Put the robot's pen down
-
+		N7.penDown();
 
 		// #4. Set the robot’s speed to 5
+		N7.setSpeed(5);
 
+		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6
+		// times (use i as the counter)
+		for (int i = 0; i < 6; i++) {
 
-		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
-
-			
 			// #6. Set the pen color to black
 
-			
+			N7.setPenColor(0, 0, 0);
 			// #7. move the robot by 50
+			N7.move(50);
 
-			
 			// #8. turn the robot to the right by 60
-			
-			
+			N7.turn(60);
+
 			// #12. set the pen color to blue for even numbers of i
+			if (i % 2 == 0) {
+				N7.setPenColor(0, 0, 255);
+			}
+			// #13. set the pen color to red for all odd numbers of i
+			else {
+				N7.setPenColor(255, 0, 0);
+			}
+
+			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
+			for (int j = 0; j < 6; j++) {
 
 			
-			// #13. set the pen color to red for all odd numbers of i
-			
-			
-			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
-			
-			
-					// #10. turn the robot to the left by 60
-		
-					
-					// #11. move the robot by 50 (play the start button)
-					
-				
-			
+
+			// #10. turn the robot to the left by 60
+			N7.turn(-60);
+
+			// #11. move the robot by 50 (play the start button)
+			N7.move(50);
+			}
 			// #14. set the pen color to black
-				
-		
+		N7.setPenColor(0,0,0);
+
 	}
 
+}
 }
