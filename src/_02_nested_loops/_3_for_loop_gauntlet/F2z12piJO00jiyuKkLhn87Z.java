1a932148043b25c02b1d367652dba1ce97d4c304
root <root@15ad6c4ee93b>
2020-07-30 01:36:09 +0000

    tyhjtyt534e2rtyru456y3wa

--- src/_02_nested_loops/_3_for_loop_gauntlet/F2z12piJO00jiyuKkLhn87Z.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/F2z12piJO00jiyuKkLhn87Z.java
@@ -4,8 +4,274 @@ public class F2z12piJO00jiyuKkLhn87Z {
 
 public static void main(String[] args) {
 	for (int i = 0; i < 100; i++) {
-		
+		System.out.println(i);
 	}
+			for (int j = 100; j > 0; j--) {
+			System.out.println(j);
+		}
+				for (int i = 2; i < 100; i++) {
+					System.out.println(i);
+				}
+					for (int i = 1; i < 99; i++) {
+						System.out.println(i);
+					}
+						for (int i = 1; i < 500; i++) {
+							
+							if(i%2==0){
+								System.out.println(i+"even");
+								}
+							else{
+								System.out.println(i+"odd");
+								}
+						}
+							for (int g = 0; g < 781; g++) {
+							if(g%7==0) {
+					
+								System.out.println(g);
+							}
+							}
+							
+int y =2008;	
+	
+				for (int i = 0; i < 12; i++) {
+					y++;
+			System.out.println(
+					"in"+ y + " I was "+ i +" years old"
+					
+					
+					
+					);}}}					
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+
+	
+	
+
 	
 	
 	
@@ -34,14 +300,4 @@ public static void main(String[] args) {
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}	
-}
+
