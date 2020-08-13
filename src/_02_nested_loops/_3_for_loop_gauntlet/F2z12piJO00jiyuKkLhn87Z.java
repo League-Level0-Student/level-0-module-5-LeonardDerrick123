a20ee82f0f85dd2304a594b27cdfc345a9e62bac
root <root@cb956564996f>
2020-08-13 01:33:30 +0000

    wefgdfSWqEzdgx

--- src/_02_nested_loops/_3_for_loop_gauntlet/F2z12piJO00jiyuKkLhn87Z.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/F2z12piJO00jiyuKkLhn87Z.java
@@ -2,302 +2,60 @@ package _02_nested_loops._3_for_loop_gauntlet;
 
 public class F2z12piJO00jiyuKkLhn87Z {
 
-public static void main(String[] args) {
-	for (int i = 0; i < 100; i++) {
-		System.out.println(i);
-	}
-			for (int j = 100; j > 0; j--) {
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			System.out.println(i);
+		}
+		for (int j = 100; j > 0; j--) {
 			System.out.println(j);
 		}
-				for (int i = 2; i < 100; i++) {
-					System.out.println(i);
-				}
-					for (int i = 1; i < 99; i++) {
-						System.out.println(i);
+		for (int i = 2; i < 100; i++) {
+			System.out.println(i);
+		}
+		for (int i = 1; i < 99; i++) {
+			System.out.println(i);
+		}
+		for (int i = 1; i < 500; i++) {
+
+			if (i % 2 == 0) {
+				System.out.println(i + "even");
+			} else {
+				System.out.println(i + "odd");
+			}
+		}
+		for (int g = 0; g < 781; g++) {
+			if (g % 7 == 0) {
+
+				System.out.println(g);
+			}
+		}
+
+		int y = 2008;
+
+		for (int i = 0; i < 12; i++) {
+			y++;
+			System.out.println("in" + y + " I was " + i + " years old"
+
+			);
+		}
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.println(i + " " + j);
+			}
+		}
+		
+		
+		
+				for (int j2 = 0; j2 < 8; j2+=3) {
+					for (int i = j2; j2 < j2+3;j2++) {
+						System.out.println(j2);
+					
 					}
-						for (int i = 1; i < 500; i++) {
-							
-							if(i%2==0){
-								System.out.println(i+"even");
-								}
-							else{
-								System.out.println(i+"odd");
-								}
-						}
-							for (int g = 0; g < 781; g++) {
-							if(g%7==0) {
-					
-								System.out.println(g);
-							}
-							}
-							
-int y =2008;	
-	
-				for (int i = 0; i < 12; i++) {
-					y++;
-			System.out.println(
-					"in"+ y + " I was "+ i +" years old"
-					
-					
-					
-					);}}}					
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
-							
+				
+				 
+			
+			}
+		}
 
-	
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	}
 
